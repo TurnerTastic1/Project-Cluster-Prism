b0027a750ca1cfd513d1ed559dab9f639e803de6
Turner Naef <Turner.Naef@gmail.com>
2022-12-25 16:27:41 -0500

    signin route and controller

--- src/controllers/auth/auth.ts
+++ src/controllers/auth/auth.ts
@@ -1,5 +1,7 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+//import jwt from 'jwt-simple';
+
 import User from '../../models/user.model';
 
 const signupController = async (req: Request, res: Response) => {
@@ -21,4 +23,16 @@ const signupController = async (req: Request, res: Response) => {
 
 };
 
-export default { signupController };
+const signinController = async (req: Request, res: Response, next: NextFunction) => {
+// Checking if user/email exists and is in DB
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Email is not associated with any known accounts');
+
+  // Checking if password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send('Invalid password');
+
+  return res.status(200).send('Signed in');
+};
+
+export default { signupController, signinController };
--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -8,4 +8,6 @@ import controller from '../../controllers/auth/auth';
 // User model to see requirments for inputs
 app.post('/signup', [ middleware.inputValidation, middleware.validateUsernameAndEmail ], controller.signupController);
 
+app.post('/signin', controller.signinController);
+
 export = app;
